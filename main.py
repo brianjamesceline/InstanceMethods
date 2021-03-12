f248bc52f0de921ab5037084a9b7e619ebf2df56
Brian James Celine <brianceline@gmail.com>
2021-03-12 15:22:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, scores = []):
+      self.scores = scores
+
+    def avg(self):
+        return round(sum(self.scores) / len(self.scores))
+
+# object / instance
+kings = Student(scores = [2,3,5,3,5,35])
+peter = Student(scores = [27,30,5,3,5,35])
+
+print(kings.avg())
+print(peter.avg())
+
+
